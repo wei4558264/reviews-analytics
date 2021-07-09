b3a1c75b2bc4d19ad5eaa2c09e1f215d72d22a16
Amywei <amywei2008@gmail.com>
2021-07-09 22:07:40 +0800

    for vision(not while)

--- read.py
+++ read.py
@@ -2,9 +2,7 @@ data = []
 with open('reviews.txt','r') as f:
 	for line in f:
 		data.append(line)
-i = 0
-x = 0
-while i < 1000000:
-	x = x + len(data[i])
-	i = i+1
-print(x/1000000)
+x = 0 #
+for d in data:
+	x = x + len(d)	
+print(x/len(data))
