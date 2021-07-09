78c37dcc8c2f11308b9d3f6c54c9c4089c1ab5b8
Amywei <amywei2008@gmail.com>
2021-07-09 22:36:18 +0800

    add filter version

--- read.py
+++ read.py
@@ -6,3 +6,11 @@ x = 0 #
 for d in data:
 	x = x + len(d)	
 print(x/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆清單長度小於100字')
+print(new[0])
+print(new[1])
